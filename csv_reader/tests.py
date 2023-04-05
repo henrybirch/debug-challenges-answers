e639682995b01427b4833a9afeb0c88f3e4da782
henrybirch <hab.b@hotmail.co.uk>
2023-04-05 17:28:21 +0100

    Add mixed_csv test

--- csv_reader/tests.py
+++ csv_reader/tests.py
@@ -40,8 +40,55 @@ class Tests(unittest.TestCase):
         self.assertEqual(result, {})
 
     def test_empty_csv_mk2(self):
-        result = result = parse_csv([])
+        result = parse_csv([])
         self.assertEqual(result, {})
+
+    def test_mixed_csv(self):
+        result = parse_csv(
+            [
+                "column_1,column_2,column_3",
+                "89,2.02,1|None|6|None",
+                "29,5.1,5|None|4|None",
+                "70,4.62,5|None|3|None",
+                "87,9.37,",
+                "73,1.75,",
+                "64,9.27,10|None|4|None",
+                "70,2.64,7|None|6|None",
+                "95,2.28,10|None|4|None",
+                "92,2.58,",
+                "79,7.02,",
+            ]
+        )
+        self.assertEqual(
+            result,
+            {
+                "column_1": [89, 29, 70, 87, 73, 64, 70, 95, 92, 79],
+                "column_2": [
+                    2.02,
+                    5.1,
+                    4.62,
+                    9.37,
+                    1.75,
+                    9.27,
+                    2.64,
+                    2.28,
+                    2.58,
+                    7.02,
+                ],
+                "column_3": [
+                    [1, None, 6, None],
+                    [5, None, 4, None],
+                    [5, None, 3, None],
+                    None,
+                    None,
+                    [10, None, 4, None],
+                    [7, None, 6, None],
+                    [10, None, 4, None],
+                    None,
+                    None,
+                ],
+            },
+        )
 
 
 if __name__ == "__main__":
